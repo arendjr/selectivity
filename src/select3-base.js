78c9c89cdb998b963ae7d17aa7b9be87d9d026a5
Arend van Beelen <arendjr@gmail.com>
2014-12-30 22:08:35 +0100

    Fix jshint errors.

--- src/select3-base.js
+++ src/select3-base.js
@@ -108,6 +108,16 @@ $.extend(Select3.prototype, {
 
         this.options = options;
 
+        function processItem(item) {
+            if (item && Select3.isValidID(item.id)) {
+                return item;
+            } else if (Select3.isValidID(item)) {
+                return { id: item, text: '' + item };
+            } else {
+                throw new Error('items array contains invalid items');
+            }
+        }
+
         for (var key in options) {
             if (options.hasOwnProperty(key)) {
                 var value = options[key];
@@ -120,15 +130,7 @@ $.extend(Select3.prototype, {
 
                 case 'items':
                     if ($.type(value) === 'array') {
-                        this.items = value.map(function(item) {
-                            if (item && Select3.isValidID(item.id)) {
-                                return item;
-                            } else if (Select3.isValidID(item)) {
-                                return { id: item, text: '' + item };
-                            } else {
-                                throw new Error('items array contains invalid items');
-                            }
-                        });
+                        this.items = value.map(processItem);
                     } else {
                         throw new Error('items must be an array');
                     }
@@ -291,8 +293,8 @@ $.fn.select3 = function(methodName, options) {
                 throw new Error('Cannot call method on element without Select3 instance');
             } else {
                 options = $.extend({}, methodName, { element: this });
-                this.select3 = new (options.multiple ? require('./select3-multiple')
-                                                     : require('./select3-single'))(options);
+                this.select3 = (options.multiple ? new (require('./select3-multiple'))(options)
+                                                 : new (require('./select3-single'))(options));
             }
         }
     });
