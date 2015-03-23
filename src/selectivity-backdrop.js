295c9e2dbc7b6c0807371d5c735619a115595ce7
Arend van Beelen <arendjr@gmail.com>
2015-03-23 20:23:29 +0100

    Move all z-index settings to CSS. Fixes #10.

--- src/selectivity-backdrop.js
+++ src/selectivity-backdrop.js
@@ -4,9 +4,6 @@ var $ = require('jquery');
 
 var SelectivityDropdown = require('./selectivity-dropdown');
 
-var BACKDROP_Z_INDEX = 9998;
-var FOREGROUND_Z_INDEX = 9999;
-
 /**
  * Methods.
  */
@@ -17,9 +14,7 @@ $.extend(SelectivityDropdown.prototype, {
      */
     addToDom: function() {
 
-        var $selectivityEl = this.selectivity.$el;
-        $selectivityEl.css({ zIndex: FOREGROUND_Z_INDEX, position: 'relative' });
-        this.$el.appendTo($selectivityEl[0].ownerDocument.body).css('zIndex', FOREGROUND_Z_INDEX);
+        this.$el.appendTo(this.selectivity.$el[0].ownerDocument.body);
     },
 
     /**
@@ -42,15 +37,7 @@ $.extend(SelectivityDropdown.prototype, {
         if (this.parentMenu) {
             $backdrop = this.parentMenu._$backdrop;
         } else {
-            $backdrop = $('<div>').addClass('.selectivity-backdrop').css({
-                background: 'transparent',
-                bottom: 0,
-                left: 0,
-                position: 'fixed',
-                right: 0,
-                top: 0,
-                zIndex: BACKDROP_Z_INDEX
-            });
+            $backdrop = $('<div>').addClass('selectivity-backdrop');
 
             $('body').append($backdrop);
         }
