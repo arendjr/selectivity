b82936ba08f14df6241262030341da6d80a23797
Konrad Jurkowski <konrad@jurkowski.me>
2015-04-09 14:21:48 +0200

    Added autoprefixer for Sass

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -5,6 +5,7 @@ var concat = require('gulp-concat');
 var csso = require('gulp-csso');
 var gulpif = require('gulp-if');
 var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var gutil = require('gulp-util');
 var argv = require('yargs').argv;
 
@@ -18,6 +19,7 @@ module.exports = function() {
             .on('error', function(error) {
                 gutil.log(gutil.colors.red('Error building CSS bundle: ') + error.toString());
             })
+            .pipe(autoprefixer({ browsers: ['last 5 versions'], cascade: false }))
             .pipe(concat('selectivity-' + argv.bundleName + (argv.minify ? '.min' : '') + '.css'))
             .pipe(gulpif(argv.minify, csso()))
             .pipe(gulp.dest('dist/'));
