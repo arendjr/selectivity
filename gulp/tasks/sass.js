e5e84c6226061d862f0c2e14744c9ab0053d9472
Konrad Jurkowski <konrad@jurkowski.me>
2015-04-09 12:47:50 +0200

    Renamed and reordered styles

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var fs = require('fs');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var csso = require('gulp-csso');
@@ -15,17 +14,7 @@ module.exports = function() {
 
     CustomBuildUtil.checkOptions(argv);
 
-    fs.writeFileSync('styles/selectivity-custom.sass', argv.modules.map(function(module) {
-        if (fs.existsSync('styles/selectivity-' + module + '.sass')) {
-            return "@import 'selectivity-" + module + "'\n";
-        } else if (fs.existsSync('styles/selectivity-' + module + '.scss')) {
-            return "@import 'selectivity-" + module + "'\n";
-        } else {
-            return '';
-        }
-    }).join(''));
-
-    return sass('styles/selectivity-custom.sass')
+    return sass('styles/selectivity.sass')
             .on('error', function(error) {
                 gutil.log(gutil.colors.red('Error building CSS bundle: ') + error.toString());
             })
