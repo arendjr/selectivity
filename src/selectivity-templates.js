4d0ddcd9fbc6c38daec636ebc5b20a0678505916
Konrad Jurkowski <konrad@jurkowski.me>
2015-04-17 19:23:17 +0200

    Added fix for <select multiple> name that misses []

--- src/selectivity-templates.js
+++ src/selectivity-templates.js
@@ -278,6 +278,9 @@ Selectivity.Templates = {
      *                mode - Mode in which select exists, single or multiple.
      */
     selectCompliance: function(options) {
+        if (!options.name.match(/\[\]$/)) {
+            options.name = options.name + '[]';
+        }
         return ('<select name="' + options.name + '"' + (options.mode === 'multiple' ? ' multiple' : '') + '></select>');
     },
 
