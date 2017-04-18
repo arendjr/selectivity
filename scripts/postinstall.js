bf46bbb978d937ee2acf17a0f43bc489829a9000
Cezar Almeida <calmeida@estantevirtual.com.br>
2017-04-18 17:38:49 -0300

    - Adding postinstall script

---
+++ scripts/postinstall.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+/* eslint no-console: 0 */
+
+'use strict';
+
+var execSync = require('child_process').execSync;
+
+var versionName = 'master';
+var releasesDir = 'release';
+var selectivityReleasePath = `${releasesDir}/selectivity`;
+var masterReleasePath = `${selectivityReleasePath}-${versionName}`;
+var npmReleasePath = `${selectivityReleasePath}-npm`;
+var distPath = 'dist';
+
+function remove(path) {
+  execSync(`rm -rf ${path}`);
+};
+
+console.log('cleaning existing files for master release');
+remove(masterReleasePath);
+remove(`${masterReleasePath}.tar.gz`);
+remove(npmReleasePath);
+
+console.log('cleaning existing dist files');
+remove(distPath);
+
+console.log('running "create-release" tool');
+execSync(`tools/create-release.js ${versionName} --clean false`);
+
+console.log('creating dist');
+execSync(`mv ${npmReleasePath} ${distPath}`);
+
+console.log('cleaning');
+remove(masterReleasePath);
+remove(`${masterReleasePath}.tar.gz`);
